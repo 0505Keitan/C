f67b9bf4e3dbd25fe53e38bd16e70ffb3a39bae5
0505Keitan <0505Keitan@gmail.com>
2019-01-28 12:02:26 +0900

    結構書いたからcommit

--- C/Program.cs
+++ C/Program.cs
@@ -6,6 +6,20 @@ namespace C {
             Console.WriteLine("Hello World!");
             Console.WriteLine(1 + 1);
             Console.WriteLine("初めてのC#スクリプトを書いたよ！");
+            int x = 0;
+            string moji = "Hello, World";
+            Console.WriteLine(x);
+            Console.WriteLine(moji);
+            moji = "こんにちは、 世界。";
+            Console.WriteLine(moji);
+            // 楽しい！
+            int[] data = new int[5];
+            data[0] = 5;
+            data[1] = 4;
+            data[2] = 3;
+            Console.WriteLine(data[0]);
+            String[] emoji = {"あああ", "いいい"};
+            Console.WriteLine(emoji[0]);
         }
     }
 }
