7daf15e11f92aacf679d031f1971254b8e403b8c
0505Keitan <0505Keitan@gmail.com>
2019-01-28 10:02:08 +0900

    First C# Commit

---
+++ C/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace C {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(1 + 1);
+            Console.WriteLine("初めてのC#スクリプトを書いたよ！");
+        }
+    }
+}
