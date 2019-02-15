2205051c8fa4e6676f084c8fc40e12a45e5b83c5
0505Keitan <0505Keitan@gmail.com>
2019-02-15 21:17:37 +0900

    Update Program.cs

--- C/Program.cs
+++ C/Program.cs
@@ -3,6 +3,7 @@
 namespace C {
     class Program {
         static void Main(string[] args) {
+            /*
             Console.WriteLine("Hello World!");
             Console.WriteLine(1 + 1);
             Console.WriteLine("初めてのC#スクリプトを書いたよ！");
@@ -20,6 +21,20 @@ namespace C {
             Console.WriteLine(data[0]);
             String[] emoji = {"あああ", "いいい"};
             Console.WriteLine(emoji[0]);
+            Console.WriteLine("--------------");
+            */
+            Console.WriteLine("あなたのお名前を教えてね");
+            string YourName = Console.ReadLine();
+            Console.WriteLine("あなたの名前は" + YourName + "です。合ってますか？");
+            string okng = Console.ReadLine();
+            if(okng == "はい"){
+                Console.WriteLine("よかった。安心しました！");
+            }else{
+                Console.WriteLine("もう一度お名前をお聞きします。");
+                Console.WriteLine("あなたの名前はなんですか？");
+                string ReYourName = Console.ReadLine();
+                Console.WriteLine("あなたの名前は" + ReYourName + "です。もう知りません。");
+            }
         }
     }
 }
